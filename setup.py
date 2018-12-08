4c8d1dae0d99789658676ed4401b87725f94c1db
JoostSijm <joostsijm@gmail.com>
2018-12-08 16:24:29 +0100

    Rename package

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Python_Supremacy1914",
+    name="supremacy1914_wrapper",
     version="0.1.0",
     author="Joost Sijm",
     author_email="joostsijm@gmail.com",
     description="Supremacy1914 API wrapper in Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/joostsijm/python_supremacy1914",
+    url="https://github.com/joostsijm/supremacy1914_wrapper",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
